fde9b13b52e9cab1f373f609b2f7e35865d7e963
unknown <viniciusbeloti7@gmail.com>
2020-09-10 12:12:57 -0300

    feat: add VideoFooter Component

---
+++ src/components/VideoFooter/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+const VideoFooter = () => {
+  return (
+    <div className="videoFooter">
+
+    </div>
+  );
+}
+
+export default VideoFooter;
