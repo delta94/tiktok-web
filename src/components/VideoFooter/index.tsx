4de2769234148878548d40eeeefbe7a81ac01edf
unknown <viniciusbeloti7@gmail.com>
2020-09-10 12:40:48 -0300

    chore: update VideoFooter styles

--- src/components/VideoFooter/index.tsx
+++ src/components/VideoFooter/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
-
+import Ticker from 'react-ticker';
+import spin from '../../assets/img/spin.png';
 import './styles.css';
 
-const VideoFooter = () => {
+interface IVideoFooter {
+  song?: string;
+}
+
+const VideoFooter = ({ song }: IVideoFooter) => {
   return (
     <div className="videoFooter">
       <div className="videoFooter__text">
@@ -11,10 +16,19 @@ const VideoFooter = () => {
         <p>Description...</p>
         <div className="videoFooter__ticker">
           <MusicNoteIcon className="videoFooter__icon" />
+
+          <Ticker mode="smooth">
+            {({ index }) => (
+              <>
+                <p>{song} song</p>
+              </>
+            )}
+          </Ticker>
         </div>
       </div>
+      <img src={spin} alt="Spin Icon" className="videoFooter__record"/>
     </div>
   );
-}
+};
 
 export default VideoFooter;
