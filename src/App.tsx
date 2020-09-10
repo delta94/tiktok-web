ed19b59e118220b9171f8874f012ccbf948eae71
unknown <viniciusbeloti7@gmail.com>
2020-09-10 12:14:29 -0300

    chore: import Video in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Video from './components/Video';
 
 function App() {
   return (
     <div className="app">
-
+        <Video />
     </div>
   );
 }
